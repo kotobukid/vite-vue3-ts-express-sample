dfa51bc9384a481a0522269612650482fd92850d
kotobukid <kotobuki@daqua.jp>
2022-07-25 17:24:02 +0900

    まずは変数へのアクセス しかしこのコード何もexportしていないのにCompositionAPIのように動いていて謎→script setupの記述によるものらしい

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,49 @@
 <script setup lang="ts">
 import {ref} from 'vue'
+import _ from 'lodash'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const reset = () => {
+
+    const highest: number = count.value
+    const interval: number = 50
+    _.map(_.range(count.value), (value: number, index: number) => {
+            setTimeout(() => {
+                count.value = value
+            }, interval * (count.value - index))
+        }
+    )
+
+    setTimeout(() => {
+        count.value = 0
+    }, interval * highest)
+}
 </script>
 
-<template>
-<h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
 
-<div class="card">
-    <button type="button" @click="count++">count is {{ count * 2 }}</button>
-    <button type="button" @click="count = 0">Reset</button>
-    <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-</div>
+.card
+    button(type="button" @click="count++") count is {{ count }}
+    button(type="button" @click="reset") Reset
+    p
+        span Edit!&nbsp;
+        code components/HelloWorld.vue
+        span to test HMR
 
-<p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-    >create-vue</a
-    >, the official Vue + Vite starter
-</p>
-<p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-</p>
-<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+p
+    span Check out
+    a(href="https://vuejs.org/guide/quick-start.html#local" target="_blank") create-vue
+    span , the official Vue + Vite starter
+
+p
+    span Install
+    a(href="https://github.com/johnsoncodehk/volar" target="_blank") Volar
+    span in your IDE for a better DX
+
+p.read-the-docs Click on the Vite and Vue logos to learn more
 </template>
 
 <style scoped>
