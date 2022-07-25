0dfc42bbc03dfdfd031390c4a2d8146bef7242d6
kotobukid <kotobuki@daqua.jp>
2022-07-25 18:16:23 +0900

    別で作ったexpressの雛形を混ぜ込み、とりあえずwebサーバーが動くように

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+export default router
